762e2f31f62e087bb654596bb63e674a82d11a11
Kirill Pasikuta <pembrock@gmail.com>
2016-03-02 06:15:51 +0300

    add hotel2room

--- admin/hotel2room.php
+++ admin/hotel2room.php
@@ -8,6 +8,32 @@
 
 require 'inc/ini.inc.php';
 
+if(isset($_POST['type']) && $_POST['type'] == 'add')
+{
+    $hid = intval($_POST['id']);
+    $rid = intval($_POST['rid']);
+    $isActive = intval($_POST['isActive']);
+    $cost_hour = intval($_POST['cost_hour']);
+    $cost_day = intval($_POST['cost_day']);
+    $description = $_POST['description'];
+    $values = array('hid' => $hid, 'rid' => $rid, 'description' => $description, 'cost_hour' => $cost_hour, 'cost_day' => $cost_day, 'isActive' => $isActive);
+    $query = $fpdo->insertInto('hotel2room')->values($values);
+    $query->execute();
+
+    header('Location: /admin/hotel2room.php?act=edit&hid=' . $hid . '&rid=' . $rid);
+}
+
+$data = array();
+if (isset($_GET['act']))
+{
+    switch($_GET['act'])
+    {
+        case 'add':
+            $data['hid'] = $_GET['hid'];
+            $data['type'] = $_GET['act'];
+    }
+}
+
 $rooms = $fpdo->from('rooms')->where('isActive', 1)->orderBy('orderBy')->fetchAll();
 
-echo $twig->render('/admin/hotel2room.html.twig', array('rooms' => $rooms));
+echo $twig->render('/admin/hotel2room.html.twig', array('rooms' => $rooms, 'data' => $data));
